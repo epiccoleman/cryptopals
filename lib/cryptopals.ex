d480000ed6734d6f95a1910e1b2d1b9f0d72a07e
Eric Coleman <eric@epiccoleman.com>
2018-10-01 10:51:25 -0400

    first two problems

--- lib/cryptopals.ex
+++ lib/cryptopals.ex
@@ -1,18 +1,20 @@
 defmodule Cryptopals do
-  @moduledoc """
-  Documentation for Cryptopals.
-  """
 
-  @doc """
-  Hello world.
+  require Bitwise
 
-  ## Examples
-
-      iex> Cryptopals.hello
-      :world
-
-  """
-  def hello do
-    :world
+  def hex_to_base64(hex_string) do
+    hex_string
+    |> Base.decode16!(case: :mixed)
+    |> Base.encode64()
   end
+
+  def fixed_xor(a, b) do
+    {:ok , decode_a} = Base.decode16(a, case: :mixed)
+    {:ok , decode_b} = Base.decode16(b, case: :mixed)
+    Enum.zip(to_charlist(decode_a), to_charlist(decode_b))
+    |> Enum.map(fn {a, b} -> Bitwise.bxor(a, b) end)
+    |> to_string()
+    |> Base.encode16()
+  end
+
 end
--- test/cryptopals_test.exs
+++ test/cryptopals_test.exs
@@ -2,7 +2,18 @@ defmodule CryptopalsTest do
   use ExUnit.Case
   doctest Cryptopals
 
-  test "greets the world" do
-    assert Cryptopals.hello() == :world
+  test "hex_to_base64 works" do
+    test_input = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+    expected = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+
+    assert Cryptopals.hex_to_base64(test_input) == expected
+  end
+
+  test "xor works" do
+    a = "1c0111001f010100061a024b53535009181c"
+    b = "686974207468652062756c6c277320657965"
+    expected = "746865206B696420646F6E277420706C6179"
+
+    assert Cryptopals.fixed_xor(a, b) == expected
   end
 end
